b3f0752df60802b7d1f15c80cf60cca86acbe50f
Thomas <thomasbourgeois2@hotmail.com>
2023-03-24 15:12:17 -0700

    updated code

--- WS2813b_Led_Strip/WS2813b_Led_Strip.ino
+++ WS2813b_Led_Strip/WS2813b_Led_Strip.ino
@@ -1,29 +1,47 @@
 #include <FastLED.h>
 
-// How many leds in your strip?
-#define NUM_LEDS 1
+// Definining pins
+#define DATA_PIN 2  // data pin
+#define POT_PIN A3
 
-// For led chips like WS2812, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-// Clock pin only needed for SPI based chipsets when not using hardware SPI
-#define DATA_PIN 3
+
+const int NUM_LEDS = 10; // number of leds
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-void setup() { 
-    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+// ********** Functions ******************
 
+int read_pot() {
+  float pot = analogRead(POT_PIN);
+
+  float scale = map(pot, 0, 915, 0, 255);
+  Serial.println(scale);
+  return scale;
 }
 
-void loop() { 
+
+// ********** Main Program ******************
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POT_PIN, INPUT);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+}
+
+
+void loop() {
   // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
+  for (int i = 0; i < 5; i++) {
+    leds[i] = CRGB::Red;
+  }
+  for (int i = 3; i < 7; i++) {
+    leds[i] = CRGB::Gold;
+  }
+  for (int i = 7; i < 10; i++) {
+    leds[i] = CRGB::Blue;
+  }
+  FastLED.setBrightness(constrain(read_pot(), 0, 255));
   FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
 }
